98ea87738f1d35800e5b366466eb7b95b7395e4b
gdeal <gdeal@expedia.com>
2018-04-13 10:23:55 -0700

    grid made

--- Conway/src/main/java/entities/Grid.java
+++ Conway/src/main/java/entities/Grid.java
@@ -1,7 +1,34 @@
 package entities;
 
 public class Grid {
-    boolean[][] grid;
+    boolean[][] currentArray;
+    boolean[][] nextArray;
 
+    int x;
+    int y;
 
+    public Grid(int x, int y) {
+        currentArray = initializeArray(x,y);
+        nextArray = initializeArray(x,y);
+    }
+
+    private boolean[][] initializeArray(int x, int y) {
+        boolean array[][] = new boolean[x][];
+        for(int i = 0; i < x; i++) {
+            array[i] = new boolean[y];
+        }
+        return array;
+    }
+
+    public boolean[][] getCurrentArray() {
+        return currentArray;
+    }
+
+    public void setCurrentArray(boolean[][] currentArray) {
+        this.currentArray = currentArray;
+    }
+
+    public void iterate() {
+        for
+    }
 }
--- Conway/src/test/java/entities/GridTest.java
+++ Conway/src/test/java/entities/GridTest.java
@@ -1,18 +1,27 @@
 package entities;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
 
 public class GridTest {
     private Grid grid;
 
     @BeforeMethod
     public void setup() {
-        grid = new Grid();
+        grid = new Grid(3, 3);
     }
 
     @Test
+    public void testNoNeighborsDies() {
+        boolean[][] array = grid.getCurrentArray();
+        array[1][1] = true;
+        grid.setCurrentArray(array);
 
+        grid.iterate();
+
+        array = grid.getCurrentArray();
+
+        Assert.assertFalse(array[1][1]);
+    }
 }
