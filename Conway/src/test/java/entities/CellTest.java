60e66fa542b3f5b43c4989bf02d3a3c62aef5872
gdeal <gdeal@expedia.com>
2018-04-13 10:12:03 -0700

    initial

---
+++ Conway/src/test/java/entities/CellTest.java
@@ -0,0 +1,7 @@
+package entities;
+
+import static org.testng.Assert.*;
+
+public class CellTest {
+    
+}
---
+++ Conway/src/test/java/entities/GridTest.java
@@ -0,0 +1,18 @@
+package entities;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class GridTest {
+    private Grid grid;
+
+    @BeforeMethod
+    public void setup() {
+        grid = new Grid();
+    }
+
+    @Test
+
+}
