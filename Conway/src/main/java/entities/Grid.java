a3bf4a4f5620f5778f478703ce692ef7347f853b
Marcel Englmaier <menglmaier@BELC02SV5V9G8WN.sea.corp.expecn.com>
2018-04-13 11:05:56 -0700

    asdf

--- Conway/src/main/java/entities/Grid.java
+++ Conway/src/main/java/entities/Grid.java
@@ -35,11 +35,11 @@ public class Grid {
             {
                 int countNeighbors = currentArray[i-1][j-1] +
                         currentArray[i][j-1] +
-                        currentArray[i-1][j-1]+
-                        currentArray[i-1][j]+
-                        currentArray[i+1][j]+
-                        currentArray[i+1][j-1]+
-                        currentArray[i+1][j]+
+                        currentArray[i+1][j-1] +
+                        currentArray[i-1][j] +
+                        currentArray[i+1][j] +
+                        currentArray[i+1][j-1] +
+                        currentArray[i+1][j] +
                         currentArray[i+1][j+1];
                 if(countNeighbors < 2)
                 {
@@ -59,5 +59,6 @@ public class Grid {
                 }
             }
         }
+        currentArray = nextArray;
     }
 }
